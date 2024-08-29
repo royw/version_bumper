846ca94dfb30e842543b33a549addd7365b2c217
Roy Wright <roy@wright.org>
2024-08-29 12:09:14 -0500

    added file not found error handling.

--- src/version_bumper/pyproject.py
+++ src/version_bumper/pyproject.py
@@ -21,13 +21,17 @@ class PyProject:
         Load the versions specified by a list of dotted keys from the pyproject.toml file.
         """
         versions: list[Version | None] = []
-        with pyproject_toml_path.open(encoding="utf-8") as f:
-            doc: TOMLDocument = tomlkit.load(f)
-            for key_dot_notation in key_dot_notation_list:
-                field: Any = doc
-                for key in key_dot_notation.split("."):
-                    field = field.get(key)
-                versions.append(Version(field) if field is not None else None)
+        try:
+            with pyproject_toml_path.open(encoding="utf-8") as f:
+                doc: TOMLDocument = tomlkit.load(f)
+                for key_dot_notation in key_dot_notation_list:
+                    field: Any = doc
+                    for key in key_dot_notation.split("."):
+                        field = field.get(key)
+                    versions.append(Version(field) if field is not None else None)
+        except FileNotFoundError as ex:
+            errmsg = f'Unable to open "{pyproject_toml_path}" for loading - File not found.'
+            raise FileNotFoundError(errmsg) from ex
         return versions
 
     @classmethod
@@ -35,16 +39,20 @@ class PyProject:
         """
         Save the version to each of the dotted keys in the given list to the pyproject.toml file.
         """
-        with pyproject_toml_path.open(encoding="utf-8") as f:
-            doc: TOMLDocument = tomlkit.load(f)
-            for key_dot_notation in key_dot_notation_list:
-                field: Any = doc
-                for key in key_dot_notation.split(".")[:-1]:
-                    field = field.get(key)
-                field.update({"version": str(version)})
+        try:
+            with pyproject_toml_path.open(encoding="utf-8") as f:
+                doc: TOMLDocument = tomlkit.load(f)
+                for key_dot_notation in key_dot_notation_list:
+                    field: Any = doc
+                    for key in key_dot_notation.split(".")[:-1]:
+                        field = field.get(key)
+                    field.update({"version": str(version)})
 
-        # write to temporary file then atomically "switch" it with the original using rename.
-        with tempfile.NamedTemporaryFile("wt", dir=pyproject_toml_path.parent, delete=False) as tf:
-            tf.write(tomlkit.dumps(doc))
-            temp_name = Path(tf.name)
-        temp_name.rename(pyproject_toml_path)
+            # write to temporary file then atomically "switch" it with the original using rename.
+            with tempfile.NamedTemporaryFile("wt", dir=pyproject_toml_path.parent, delete=False) as tf:
+                tf.write(tomlkit.dumps(doc))
+                temp_name = Path(tf.name)
+            temp_name.rename(pyproject_toml_path)
+        except FileNotFoundError as ex:
+            errmsg = f'Unable to open "{pyproject_toml_path}" for saving - File not found.'
+            raise FileNotFoundError(errmsg) from ex
